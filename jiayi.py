c04e9f21a31b29d48e89a094bf982fb359ded8af
jiayishi8 <jshi100@ucsc.edu>
2025-02-19 17:38:19 -0800

    Data cleansing.

--- jiayi.py
+++ jiayi.py
@@ -5,17 +5,20 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import os
+from pathlib import Path
+from PIL import Image
+import numpy as np
 
 
 
-csv_path = "C:/Users/theet/OneDrive/art-dataset/artists.csv"
-df = pd.read_csv(csv_path)
+
+df = pd.read_csv("artists.csv")
 
 print(df.head())
 
 
 
-art_path = "C:/Users/theet/OneDrive/art-dataset/images/images"
+art_path = Path('images/images')
 
 #this resizes the images to standard 224,224
 transform = transforms.Compose([
@@ -34,7 +37,7 @@ for images, labels in train_loader:
 
 
 # Print what's inside the image folder
-print("Folders inside resized:", os.listdir(art_path))
+print("Folders inside images:", os.listdir(art_path))
 
 artist_folders = [f for f in os.listdir(art_path) if os.path.isdir(os.path.join(art_path, f))]
 print("Filtered artist folders:", artist_folders)
@@ -59,3 +62,28 @@ plt.ylabel("Number of Artworks")
 plt.title("Number of Artworks per Artist")
 
 plt.show()
+
+
+
+
+
+"""# Displays images.
+for idx, image in enumerate(images):
+    image = image.permute(1, 2, 0).numpy()  # Rearrange dimensions and convert to NumPy
+    image = (image * 255).astype(np.uint8)  # Convert from [0,1] range to [0,255] for visualization
+
+    plt1 = plt.subplot(5, 8, idx+1)
+    plt1.imshow(image)
+    plt1.set_title(labels[idx].item())  # Convert tensor to Python int
+    plt1.axis('off')
+
+plt.tight_layout()
+plt.show()"""
+
+df = df.dropna(ignore_index=True) #drops all null values, also resets the index after dropping rows with missing values
+df = df.drop_duplicates(ignore_index=True) #drops all duplicate values, also resets the index after dropping rows with missing values
+df = df.drop(columns=['bio', 'wikipedia'])
+print(df['nationality'].unique())
+print(df['genre'].unique())
+print(df['years'].unique())
+print(df.head())
